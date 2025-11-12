0a9cd006269031cad6fa4019bdbf44fd4e1a54b3
Tharsshen <107364252+Tharsshen@users.noreply.github.com>
2025-11-12 13:42:22 +0800

    Update main.py

--- Rock Paper Siccors AI/main.py
+++ Rock Paper Siccors AI/main.py
@@ -7,7 +7,7 @@ import pygame
 from tkinter import *
 from tkvideo import tkvideo
 
-# Initializee pygame mixer for music and soundsssssssssss 
+# Initializee pygame mixer for music and soundssssssssssss 
 pygame.mixer.init()
 
 # Load sound effectss
