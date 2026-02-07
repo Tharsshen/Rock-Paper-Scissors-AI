c1a9c04ba773cbbaceef9cffe9ec7c954101c422
Tharsshen <107364252+Tharsshen@users.noreply.github.com>
2026-02-07 15:08:22 +0800

    Fix typos in comments and sound effect loading

--- Rock Paper Siccors AI/main.py
+++ Rock Paper Siccors AI/main.py
@@ -7,7 +7,7 @@ import pygame
 from tkinter import *
 from tkvideo import tkvideo
 
-# Initializee pygame mixer for music and soundssssssssssssssssssssssssssssssssssss 
+# Initializee pygame mixer for music and sounds 
 pygame.mixer.init()
 
 # Load sound effectss
